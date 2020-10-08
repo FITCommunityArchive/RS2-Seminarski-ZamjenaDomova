32af5de486c7a9586fd564c8faaf630d2de6d7e1
Anja <anja.sunjic@gmail.com>
2020-10-08 15:38:44 +0200

    Wishlist - Add or Remove items from Saved items page

--- ZamjenaDomova.Mobile/ZamjenaDomova.Mobile/Pages/WishlistPage.xaml.cs
+++ ZamjenaDomova.Mobile/ZamjenaDomova.Mobile/Pages/WishlistPage.xaml.cs
@@ -37,7 +37,7 @@ namespace ZamjenaDomova.Mobile.Pages
         private void lvListings_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             var listingId = (e.SelectedItem as Model.ListingModel).ListingId;
-            Navigation.PushAsync(new OglasDetaljiPage(listingId, true));
+            Navigation.PushAsync(new OglasDetaljiPage(listingId, false));
         }
     }
 }
