3ae3b91e0d2d7ac5bbf6a36e4159ed5cfd083467
Anja <anja.sunjic@gmail.com>
2020-10-12 09:28:34 +0200

    Filter Page

--- ZamjenaDomova.Mobile/ZamjenaDomova.Mobile/Pages/FilterPage.xaml.cs
+++ ZamjenaDomova.Mobile/ZamjenaDomova.Mobile/Pages/FilterPage.xaml.cs
@@ -6,15 +6,63 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZamjenaDomova.Controls;
+using ZamjenaDomova.Model;
 
 namespace ZamjenaDomova.Mobile.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FilterPage : ContentPage
     {
+        public SelectableObservableCollection<SelectableItem<Model.AmenityModel>> AmenitiesItems { get; set; }
+        private double StepValue = 1.0;
         public FilterPage()
         {
             InitializeComponent();
+            GetAmenities();
         }
+        private async void BtnFilter_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void sPersons_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var newStep = Math.Round(e.NewValue / StepValue);
+
+            sPersons.Value = newStep * StepValue;
+            lblPersons.Text = sPersons.Value.ToString();
+        }
+
+        private void sBeds_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var newStep = Math.Round(e.NewValue / StepValue);
+
+            sBeds.Value = newStep * StepValue;
+            lblBeds.Text = sBeds.Value.ToString();
+        }
+
+        private void sBathrooms_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var newStep = Math.Round(e.NewValue / StepValue);
+
+            sBathrooms.Value = newStep * StepValue;
+            lblBathrooms.Text = sBathrooms.Value.ToString();
+        }
+        private async void GetAmenities()
+        {
+            var amenities = await APIService.GetAmenities();
+            List<AmenityModel> amenitiesModels = amenities
+                .Select(x => new AmenityModel
+                {
+                    AmenityId = x.AmenityId,
+                    Name = x.Name
+                }).ToList();
+            var amenitiesItems = amenitiesModels
+                .Select(x => new SelectableItem<AmenityModel>(x))
+                .ToList();
+            AmenitiesItems = new SelectableObservableCollection<SelectableItem<AmenityModel>>(amenitiesItems);
+            lvAmenities.ItemsSource = AmenitiesItems;
+        }
+
     }
 }
